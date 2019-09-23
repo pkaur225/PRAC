972613ee6fdaac22706fb2bff15e3995671f7f3e
Parwinder Kaur <parwinderkaur@Parwinders-MBP.fios-router.home>
2019-09-23 16:06:11 -0400

    removed forth variable

--- test.cc
+++ test.cc
@@ -1,7 +1,7 @@
- test file to run multiplication of four variables X, Y, Z and a
+ test file to run multiplication of three variables X, Y and Z
  Values 
  X= 4
  Y= 8
  Z= 3
- a= 9
- Multiplication = X*Y*Z*a
+Multiplication = X*Y*Z
+Answer= 96
